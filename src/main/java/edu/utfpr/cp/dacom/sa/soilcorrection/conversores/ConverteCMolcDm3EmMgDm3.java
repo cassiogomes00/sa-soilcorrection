2e45871189f9ad2c60ebfe1ffcc5078f42c37fe8
Cassio Pereira <cassiogomes00@gmail.com>
2021-10-11 11:56:26 -0300

    Agrupar conversores
    As classes que somente implementam a interface Conversao foram agrupadas juntos da interface, pois so possuem relacao com ela

--- src/main/java/edu/utfpr/cp/dacom/sa/soilcorrection/conversores/ConverteCMolcDm3EmMgDm3.java
+++ src/main/java/edu/utfpr/cp/dacom/sa/soilcorrection/conversores/ConverteCMolcDm3EmMgDm3.java
@@ -1,8 +1,9 @@
-package edu.utfpr.cp.dacom.sa.soilcorrection;
+package edu.utfpr.cp.dacom.sa.soilcorrection.conversores;
 
 import lombok.NonNull;
 
-public class ConverteMgDm3EmKgHa implements Conversao<Double, Double> {
+public class ConverteCMolcDm3EmMgDm3
+        implements Conversao<Double, Double> {
 
   @Override
   public Double converte(@NonNull Double valor) {
@@ -11,6 +12,7 @@ public class ConverteMgDm3EmKgHa implements Conversao<Double, Double> {
       throw new IllegalArgumentException();
     }
 
-    return valor * 2;
+    return valor * 391;
   }
+
 }
--- src/main/java/edu/utfpr/cp/dacom/sa/soilcorrection/conversores/ConverteKgHaEmK2O.java
+++ src/main/java/edu/utfpr/cp/dacom/sa/soilcorrection/conversores/ConverteKgHaEmK2O.java
@@ -1,4 +1,4 @@
-package edu.utfpr.cp.dacom.sa.soilcorrection;
+package edu.utfpr.cp.dacom.sa.soilcorrection.conversores;
 
 import lombok.NonNull;
 
